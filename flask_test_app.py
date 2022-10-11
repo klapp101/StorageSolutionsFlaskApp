95824547ea128f7c012f6d0c51ddf63b4adf5eaf
Ryan Klapper <47117306+klapp101@users.noreply.github.com>
2022-10-11 10:08:23 -0700

    Changed to POST request
    Since we're submitting a form, we want to be using a POST request. This script will eventually run an API call and that data needs to be sent to that endpoint as a parameter in order to successfully run the API call

--- flask_test_app.py
+++ flask_test_app.py
@@ -10,15 +10,17 @@ data = (
     ('Michael','Data Engineer','$120,000')
 )
 
+test_data = {'name':'ryan'}
+
 @app.route('/')
 def table():
     return render_template('table.html',headings=headings,data=data)
 
-@app.route('/button', methods = ['GET'])
+@app.route('/button', methods = ['POST'])
 def my_link():
-#   person = request.form.get('human')
-    if request.method == 'GET':
-        return 'Board has been created for: ' + request.args.get('agent_id')
+    if request.method == 'POST':
+        result = request.form
+        return result
 
 if __name__ == '__main__':
   app.run(debug=True)
